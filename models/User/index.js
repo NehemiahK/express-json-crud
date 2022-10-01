8d107b3eea5eecff487a14a344f067f62417c056
Nehemiah Kivelevitz <nehemiah@Nehemiahs-MacBook-Air.local>
2022-10-01 22:38:44 +0300

    create init user model

---
+++ models/User/index.js
@@ -0,0 +1,24 @@
+import {readFile, writeFile} from 'fs/promises'
+
+export class User {
+    constructor(name, email, password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+      }
+
+      async getDb(){
+        const db = await readFile('models/User/user.json', 'utf8')
+        return JSON.parse(db)
+    }
+
+    async save(){
+        const db = await this.getDb();
+        const id = db.length + 1;
+        const newItem = {createdAt: new Date(), name: this.name, email: this.email, password: this.password, id}
+        db.push(newItem)
+        await writeFile('models/User/user.json', JSON.stringify(db), 'utf8')
+        return newItem;
+    }
+}
+
