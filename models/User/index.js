175f7736d59ed23b6b87907c32bd2282a80ebdcd
oluwatomisin780 <tomisinoyediran@gmail.com>
2022-10-20 15:14:54 +0100

    added jwt auth

--- models/User/index.js
+++ models/User/index.js
@@ -1,26 +1,35 @@
-import {readFile, writeFile} from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises';
 import { hashPassword } from '../../services/PasswordHelper.js';
-
+const jwt = require('jsonwebtoken');
+//@jwt authentication
+const generateJwt = (id) => {
+  jwt.sign({ id }, 'jwt_secret', { expiresIn: '1hr' });
+};
 export class User {
-    constructor(name, email, password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-      }
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+  async getDb() {
+    const db = await readFile('models/User/user.json', 'utf8');
+    return JSON.parse(db);
+  }
 
-      async getDb(){
-        const db = await readFile('models/User/user.json', 'utf8')
-        return JSON.parse(db)
-    }
-
-    async save(){
-        const db = await this.getDb();
-        const id = db.length + 1;
-        const password = await hashPassword(this.password)
-        const newItem = {createdAt: new Date(), name: this.name, email: this.email, password, id}
-        db.push(newItem)
-        await writeFile('models/User/user.json', JSON.stringify(db), 'utf8')
-        return newItem;
-    }
+  async save() {
+    const db = await this.getDb();
+    const id = db.length + 1;
+    const password = await hashPassword(this.password);
+    const newItem = {
+      createdAt: new Date(),
+      token: generateJwt(id),
+      name: this.name,
+      email: this.email,
+      password,
+      id,
+    };
+    db.push(newItem);
+    await writeFile('models/User/user.json', JSON.stringify(db), 'utf8');
+    return newItem;
+  }
 }
-
